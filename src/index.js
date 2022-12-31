090f44a80a3672de7a197f39004fe1fa92308f24
mosidrum <mosiokanga@gmail.com>
2022-12-31 19:45:03 +0100

    a simple time component to add to any project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './time.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Clock extends React.Component {
+
+	state = {time: new Date().toLocaleTimeString () };
+
+	componentDidMount(){
+	     setInterval(() =>{
+		this.setState({time: new Date().toLocaleTimeString() })
+
+	 }, 1000)
+	}
+
+	render(){
+
+	return (
+	    <div className = 'time'>
+		<h1>The time is: {this.state.time}</h1>
+	    </div>
+	  );
+	}
+
+}
+
+ReactDOM.render(<Clock />, document.querySelector('#root'))
